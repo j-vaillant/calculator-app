f1f0f58fcf0a8000193c5cc0c79d7da1a266e55e
junain <julien.v91@free.fr>
2018-09-01 16:06:51 +0200

    prepare dev env

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Calculator from './container/Calculator';
+
+export default () => {
+  return (
+    <Calculator />
+  );
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000
+    },
+    devtool : 'source-map',
+    module: {
+        rules: [{
+        test: /\.js$/,
+         exclude: /(node_modules)/,
+         use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ["babel-preset-env", "babel-preset-react"],
+                 plugins: ["transform-class-properties", "transform-object-rest-spread"],
+            }
+         }
+        }]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        'template': "./sandbox/index.html",
+        'inject': "body"
+      })
+  ]   
+}
