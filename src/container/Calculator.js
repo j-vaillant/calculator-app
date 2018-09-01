3104d9540da590433e56d304fb56b236defd44c9
junain <julien.v91@free.fr>
2018-09-01 16:06:51 +0200

    prepare dev env

---
+++ src/container/Calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Calculator = class Calculator extends Component {
+  render() {
+    return (
+      <span>Hello world !</span>
+    );
+  }
+};
+
+export default Calculator;
